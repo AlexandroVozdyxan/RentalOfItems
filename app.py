34202b9442012f3e90426423c4c403eb522d142c
vovav <vovavitka10@gmail.com>
2025-01-31 00:09:13 +0200

    class DbHandler select, insert

--- app.py
+++ app.py
@@ -4,10 +4,19 @@ from flask import Flask, render_template, redirect, session
 from flask import request
 import sqlite3
 
-from werkzeug.security import check_password_hash
 
 app = Flask(__name__)
 app.secret_key = 'FEh3487duH3&*#HS#d98H#d'
+
+
+def login_required(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if 'user' not in session:
+            return redirect('/login')
+        return func(*args, **kwargs)
+    return wrapper
+
 def dict_factory(cursor, row):
     d = {}
     for idx, col in enumerate(cursor.description):
@@ -25,36 +34,59 @@ class Db_open_close(object):
         self.con.commit()
         self.con.close()
 
+class DbHandler:
+    db_file = "db1.db"
 
-def login_required(func):
-    @wraps(func)
-    def wrapped(*args, **kwargs):
-        if 'user_id' not in session:
-            return redirect('/login')
-        return func(*args, **kwargs)
-    return wrapped
+    def select(self, table_name, filter_dict=None):
+        if filter_dict is None:
+            filter_dict = {}
+        with Db_open_close(self.db_file) as db_cur:
+            query = f'SELECT * FROM {table_name}'
+            if filter_dict:
+                query += ' WHERE '
+                itms = []
+                for key, value in filter_dict.items():
+                    itms.append(f'{key} = ?')
+                query += ' AND '.join(itms)
+
+                db_cur.execute(query, tuple(value for value in filter_dict.values()))
+            else:
+                db_cur.execute(query)
+            result = db_cur.fetchall()
+            print(f"Query: {query}")
+            print(f"Result: {result}")
+            return result
+
+    def insert(self, table_name, data_dict):
+        with Db_open_close(self.db_file) as db_cur:
+            query = f'INSERT INTO {table_name} ( '
+            query += ','.join(data_dict.keys())
+            query += ' ) VALUES ( '
+            query += ','.join([f':{itms}' for itms in data_dict.keys()])
+            query += ' )'
+            db_cur.execute(query, data_dict)
+
+db_connector = DbHandler()
+
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template('index.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'GET':
         return render_template('login.html')
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-
-        with Db_open_close('db1.db') as db_cur:
-            db_cur.execute('''SELECT id, full_name FROM user WHERE login = ? AND password = ?''',
-                           (username, password))
-            user = db_cur.fetchone()
-            if user:
-                session['user_id'] = user['id']
-                session['full_na,e'] = user['full_name']
-                return 'Logged in successfully!'
-            else:
-                return 'Invalid username or password!', 401
+        user_data = db_connector.select(
+            'user', {'login': username, 'password': password})
+        if user_data:
+            session['user'] = user_data[0]['id']
+            return redirect('/profile')
+        else:
+            return render_template('login.html', error="Invalid username or password")
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'GET':
@@ -62,38 +94,34 @@ def register():
     if request.method == 'POST':
         with Db_open_close('db1.db') as db_cur:
             form_data = request.form
-            db_cur.execute('''INSERT INTO user
-            (login, password, ipn, full_name, contacts, photo, passport)
-            VALUES (?, ?, ?, ?, ?, ?, ?)''',
-                           (form_data['login'], form_data['password'], form_data['ipn'], form_data['full_name'],
-                           form_data['contacts'], form_data['photo'], form_data['passport']
-                           ))
-
+            db_connector.insert('user', form_data)
         return redirect('/login')
 
+
 @app.route('/logout', methods=['GET', 'POST', 'DELETE'])
-@login_required
 def logout():
-    session.pop('user_id', None)
+    session.pop('user', None)
     return redirect('/login')
 
 @app.route('/profile', methods=['GET', 'PUT', 'PATCH', 'DELETE'])
 @login_required
 def profile():
-    if session.get('user_id') is None:
-        return redirect('/login')
-    if request.method == 'GET':
-        with Db_open_close('db1.db') as db_cur:
-            query = '''SELECT full_name FROM user WHERE login = ?'''
-            db_cur.execute(query, (session['user_id'],))
-            full_name = db_cur.fetchone()['full_name']
-        return render_template('user.html', full_name=full_name)
-    if request.method == 'PUT':
-        return 'PUT'
-    if request.method == 'PATCH':
-        return 'PATCH'
-    if request.method == 'DELETE':
-        return 'DELETE'
+   if request.method == 'GET':
+       user_data = db_connector.select('user', {'id': session['user']})
+
+       if user_data:
+           user_data = user_data[0]
+
+           return render_template('user.html', user=user_data)
+       else:
+            return redirect('/login')
+
+   if request.method == 'PUT':
+       return 'PUT'
+   if request.method == 'PATCH':
+       return 'PATCH'
+   if request.method == 'DELETE':
+       return 'DELETE'
 
 @app.route('/profile/favorites', methods=['GET', 'POST', 'DELETE'])
 def favorites():
@@ -123,80 +151,70 @@ def search_history():
 @app.route('/items', methods=['GET', 'POST'])
 def items():
     if request.method == 'GET':
-        with Db_open_close('db1.db') as db_cur:
-            db_cur.execute("SELECT * FROM item")
-            items = db_cur.fetchall()
-        return render_template('items.html', items=items)
+        items_data = db_connector.select('item')
+        print(f"Items data: {items_data}")
+        return render_template('items.html', items=items_data)
+
     if request.method == 'POST':
-        if session.get('user_id') is None:
+        if session.get('user') is None:
             return redirect('/login')
-        with Db_open_close('db1.db') as db_cur:
+        query_args = request.form
+        query = dict(query_args)
+        query['owner'] = session['user']
 
-            user_login = session['user_id']
-            db_cur.execute("select id from user where login = ?", (user_login,))
-            user_id = db_cur.fetchone()['id']
+        db_connector.insert('item', query)
 
-            query_args = dict(request.form)
-            query_args['owner'] = user_id
-
-            db_cur.execute('''INSERT INTO item (photo, name, description, price_hour, price_day, price_week, price_month, owner)
-            VALUES (:photo, :name, :description, :price_hour, :price_day, :price_week, :price_month, :owner)''', query_args)
         return redirect('/items')
+
 
 @app.route('/items/<item_id>', methods=['GET', 'PUT', 'DELETE'])
 def item(item_id):
     if request.method == 'GET':
-        return render_template('oneitemform.html', item_id=item_id)
-    if request.method == 'PUT':
-        return f'PUT {item_id}'
+        item_data = db_connector.select('item', {'id': item_id})
+        if item_data:
+            return render_template('item.html', item=item_data[0])
+        else:
+            return "Item not found", 404
     if request.method == 'DELETE':
-        if session.get('user_id') is None:
-            return redirect('/login')
-        return f'DELETE {item_id}'
+        return 'DELETE'
 
 @app.route('/leasers', methods=['GET'])
 def leasers():
     if request.method == 'GET':
-        return 'GET'
+        with Db_open_close('db1.db') as db_cur:
+            db_connector.select('leaser')
+            leaser_data = db_cur.fetchall()
+            return render_template('user.html', user=leaser_data)
+
 
 @app.route('/leasers/<leaser_id>', methods=['GET'])
 def leaser(leaser_id):
     if request.method == 'GET':
-        return f'GET: {leaser_id}'
+        with Db_open_close('db1.db') as db_cur:
+            db_connector.select('leaser', {'leaser_id': leaser_id})
+        return 'GET'
 
 @app.route('/contracts', methods=['GET', 'POST'])
 @login_required
 def contracts():
     if request.method == 'GET':
         with Db_open_close('db1.db') as db_cur:
-            db_cur.execute("SELECT * FROM contract")
+            db_connector.select('contract', {'leaser': session['user']})
             contracts = db_cur.fetchall()
-        return render_template('contracts.html', contracts=contracts)
-        return 'GET'
+            return render_template('contract.html', contracts=contracts)
     if request.method == 'POST':
-        with Db_open_close('db1.db') as db_cur:
-            db_cur.execute("select id from user where login = ?", (session['user_id'],))
-            my_id = db_cur.fetchone()['id']
-            taker_id = my_id
-            item_id = request.form['item']
-            # 1
-            leaser_id = request.form['leaser']
-            # 2
-            db_cur.execute("select * from item where id = ?", (item_id,))
-            leaser_id = db_cur.fetchone()['owner']
-
-            contract_status = 'pending'
-
-            query_args = (request.form['text'], request.form['start_day'], request.form['end_day'], leaser_id, taker_id, item_id, contract_status)
-            inser_query = '''insert into contcract (text, start_day, end_day, leaser, taker, item, status) values (/, ?, ?, ?, ?, ?, ?)'''
-            db_cur.execute(inser_query, query_args)
-
-        return 'POST'
+           with Db_open_close('db1.db') as db_cur:
+                form_data = request.form
+                db_connector.insert('contract', form_data)
+                return redirect('/')
 
 @app.route('/contracts/<contract_id>', methods=['GET', 'PUT', 'PATCH'])
 def contracts_id(contract_id):
     if request.method == 'GET':
-        return f'GET: {contract_id}'
+        with Db_open_close('db1.db') as db_cur:
+            db_connector.select('contract', {'contract_id': contract_id})
+            contract = db_cur.fetchone()
+            return render_template('contract.html', contract=contract)
     if request.method == 'PUT':
         return f'PUT: {contract_id}'
     if request.method == 'PATCH':
@@ -211,33 +229,25 @@ def search():
 
 @app.route('/complaints', methods=['GET', 'POST'])
 def complaints():
-    if request.method == 'GET':
-        return render_template('feedback.html')
     if request.method == 'POST':
         with Db_open_close('db1.db') as db_cur:
             form_data = request.form
-            db_cur.execute('''INSERT INTO feedback
-            (author, user, text, grade, contract)
-            VALUES (?, ?, ?, ?, ?)''',
-            (form_data['author'], form_data['user'],
-             form_data['text'], form_data['grade'],
-             form_data['contract'])
-                           )
-        return render_template('complain_sub.html')
+            db_connector.insert('feedback', form_data)
 
-@app.route('/complaints/<complaint_id>', methods=['GET', 'PUT', 'DELETE'])
-def complaints_id(complaint_id):
-    if request.method == 'GET':
-        return f'GET: {complaint_id}'
-    if request.method == 'PUT':
-        return f'PUT: {complaint_id}'
-    if request.method == 'DELETE':
-        return f'DELETE: {complaint_id}'
+            return redirect('/')
+
 
 @app.route('/compare', methods=['GET', 'PUT'])
 def compare():
     if request.method == 'GET':
-        return 'GET'
+        item1_id = request.args.get('item1')
+        item2_id = request.args.get('item2')
+        if item1_id and item2_id:
+            item1 = db_connector.select('item', {'id': item1_id})
+            item2 = db_connector.select('item', {'id': item2_id})
+            if item1 and item2:
+                return render_template('compare.html', item=item1[0], item2=item2[0])
+        return "Items not found", 404
     if request.method == 'PUT':
         return 'PUT'
 
